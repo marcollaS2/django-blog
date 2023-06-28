b9fe88672a5edc219e72ca289dac9a93ac4871f2
Marcolla <marcosanselmo12br@gmail.com>
2023-06-28 11:13:06 -0300

    Introdução aos templates Django

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Incluir a classe HttpResponse.
+from django.http import HttpResponse
+
+# Define uma function view chamada index.
+def index(request):
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+
+# Define uma function view chamada ola.
+def ola(request):
+    return HttpResponse('Olá Django')
--- webapp/settings.py
+++ webapp/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for webapp project.
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
